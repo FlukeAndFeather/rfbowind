26e6ed62046177a19dacf067e599f6f7f791adec
Max Czapanskiy <maxczap@stanford.edu>
2023-01-12 12:37:12 -0800

    Trip report

--- _targets.R
+++ _targets.R
@@ -1,7 +1,7 @@
 # Load packages required to define the pipeline:
 library(here)
 library(targets)
-# library(tarchetypes) # Load other packages as needed. # nolint
+library(tarchetypes)
 
 # Set target options:
 tar_option_set(
@@ -21,28 +21,24 @@ tar_source()
 list(
   # Read and clean tracks
   tar_target(
-    name = rfbo_path,
-    command = here("analysis", "data", "raw_data", "RFBO_eObs_clean.csv"),
+    rfbo_path,
+    here("analysis", "data", "raw_data", "RFBO_eObs_clean.csv"),
     format = "file"
   ),
+  tar_target(rfbo_tracks_raw, read_tracks(rfbo_path)),
   tar_target(
-    name = rfbo_tracks_raw,
-    command = read_tracks(rfbo_path)
-  ),
-  tar_target(
-    name = rfbo_tracks_clean,
-    command = rfbo_tracks_raw %>%
+    rfbo_tracks_clean,
+    rfbo_tracks_raw %>%
       label_trips(colony_dist_km = 50, duration_hr = 2) %>%
       rediscretize(time_step = 120) %>%
       quality_filter(max_gaps = 1)
   ),
   # Fit HMM and decode states
+  tar_target(rfbo_hmm, fit_hmm(rfbo_tracks_clean)),
   tar_target(
-    name = rfbo_hmm,
-    command = fit_hmm(rfbo_tracks_clean)
+    rfbo_decoded,
+    mutate(rfbo_tracks_clean, state = viterbi(rfbo_hmm))
   ),
-  tar_target(
-    name = rfbo_decoded,
-    command = mutate(rfbo_tracks_clean, state = viterbi(rfbo_hmm))
-  )
+  # Trip report
+  tar_quarto(trip_report, here::here("analysis", "workflow", "01_trips.qmd"))
 )
